aaf5b5f668c3ef4841401d814e26a78373503a93
Xaime Pardal <xaime_pardal@outlook.com>
2022-09-30 19:23:02 +0200

    vamos carallo

--- reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
+++ reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
@@ -1,13 +1,18 @@
 from scrapy import Spider, Request
 
+
 class SteamSpider(Spider):
     name = "steam"
 
     def start_requests(self):
-        urls = [""]
+        urls = [
+            "https://store.steampowered.com/app/787810/Rogue_Heroes_Ruins_of_Tasos/"]
         for url in urls:
             yield Request(url=url, callback=self.parse)
 
-    def parse(self, response, **kwargs):
-        page = response.url
-        print(page)
+    def parse(self, response):
+        page = response.url.split("/")[2]
+        filename = f'steam-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Guardado fichero {filename}')
