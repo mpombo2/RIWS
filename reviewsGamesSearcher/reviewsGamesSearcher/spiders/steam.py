e329849657c2e1237d7d649740ec8c88efaebe8a
Xaime Pardal <xaime_pardal@outlook.com>
2022-10-14 19:58:34 +0200

    a ver funca

--- reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
+++ reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
@@ -13,19 +13,29 @@ class SteamSpider(Spider):
             "https://store.steampowered.com/app/1551360/Forza_Horizon_5/"]
 
         script = """
-    function main(splash, args)
-        splash:go(args.url)
+    function main(splash)
+        splash.images_enabled = false
+        local num_scrolls = 10
+        local scroll_delay = 1.0
+
         local scroll_to = splash:jsfunc("window.scrollTo")
-        scroll_to(0, document.body.scrollHeight)
-        return {png=splash:png()}
+        local get_body_height = splash:jsfunc(
+            "function() {return document.body.scrollHeight;}"
+        )
+        assert(splash:go(splash.args.url))
+        splash:wait(splash.args.wait)
+
+        for _ = 1, num_scrolls do
+            scroll_to(0, get_body_height())
+            splash:wait(scroll_delay)
+        end
+        return splash:html()
     end
 """
 
         for url in urls:
             # yield Request(url=url, callback=self.parse)
-            yield SplashRequest(url=url, callback=self.parse,
-                                args={'wait': 5},
-                                )
+            yield SplashRequest(url=url, callback=self.parse, endpoint='render.html', args={'lua_source': script} )
 
     def parse(self, response):
         page = response.url.split("/")[2]
