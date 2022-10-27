6245978ba2b113383cdc71d42c9be1082fc17150
Xaime Pardal <xaime_pardal@outlook.com>
2022-10-27 21:16:49 +0200

    feito carajo

--- reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
+++ reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
@@ -6,47 +6,54 @@ from scrapy_splash import SplashRequest
 class SteamSpider(Spider):
     name = "steam"
 
+    script = """
+function main(splash, args)
+    splash.images_enabled = false
+    local scroll_delay = 1.0
+    local scroll_to = splash:jsfunc("window.scrollTo")
+    local get_body_height = splash:jsfunc("function() {return document.body.scrollHeight;}")
+    assert(splash:go(splash.args.url))
+    splash:wait(splash.args.wait)
+    for _ = 1, splash.args.num_scrolls do
+        scroll_to(0, get_body_height())
+        splash:wait(scroll_delay)
+    end
+    return splash:html()
+  
+end
+"""
+
     def start_requests(self):
         urls = [
             # "https://store.steampowered.com/app/787810/Rogue_Heroes_Ruins_of_Tasos/",
             # "https://steamcommunity.com/app/787810/reviews/?browsefilter=toprated&snr=1_5_100010_",
             "https://store.steampowered.com/app/1551360/Forza_Horizon_5/"]
 
-        script = """
-function main(splash, args)
-    splash.images_enabled = false
-    local num_scrolls = 10
-    local scroll_delay = 1.0
-    local scroll_to = splash:jsfunc("window.scrollTo")
-    local get_body_height = splash:jsfunc("function() {return document.body.scrollHeight;}")
-    assert(splash:go(splash.args.url))
-    splash:wait(splash.args.wait)
-    for _ = 1, num_scrolls do
-        scroll_to(0, get_body_height())
-        splash:wait(scroll_delay)
-    end
-    local drop_button = splash:select_all(".view_all_reviews_btn")[0]
-    local children = drop_button:GetChildren()
-    for i = 1, #children do
-        print(i, children[i].Name)
-    end
-    
-    splash:wait(splash.args.wait)
-    return {
-    html = splash:html(),
-    png = splash:png(),
-  }
-end
-"""
         for url in urls:
-            # yield Request(url=url, callback=self.parse)
-            yield SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'lua_source': script, 'wait': 2})
+            yield SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'lua_source': self.script, 'wait': 2, 'num_scrolls': 5})
 
     def parse(self, response):
-        page = response.url.split("/")[2]
-        filename = f'steam-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Guardado fichero {filename}')
+        href = response.css(".view_all_reviews_btn > a::attr(href)").get()
+        yield SplashRequest(url=href, callback=self.parse_reviews, endpoint='execute', args={'lua_source': self.script, 'wait': 2, 'num_scrolls': 20})
 
-        json.loads(response.text)
+    def parse_reviews(self, response):
+        authors = response.css(
+            ".apphub_CardContentAuthorName > a::text").getall()
+        for author in authors:
+            print(author)
+
+        hours = response.css(".hours::text").getall()
+        for hour in hours:
+            print(hour)
+
+        dates = response.css(".date_posted::text").getall()
+        for date in dates:
+            print(date)
+
+        ranking = response.css(".title::text").getall()
+        for rank in ranking:
+            print(rank)
+
+        reviews = response.css(".apphub_CardTextContent::text").getall()
+        for review in reviews:
+            print(review)
