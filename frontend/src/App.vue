33432285d36bf76dc80d63970e729701ea98e0f7
Xaime Pardal <xaime_pardal@outlook.com>
2022-11-14 18:58:35 +0100

    web

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-app-bar app flat color="white">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <span class="text-h4"> Titulo </span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-switch hide-details></v-switch>
+    </v-app-bar>
+    <v-main class="mainContainer d-flex align-center">
+      <MainPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+export default {
+  name: "App",
+  components: {
+    MainPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+.mainContainer {
+  background-color: #eeeeee;
+}
+</style>
---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-sheet rounded class="mainPageSheet">
+
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+};
+</script>
+
+<style scoped>
+.mainPageSheet {
+  margin: auto;
+  width: 95vw;
+  height: 85vh;
+}
+</style>
