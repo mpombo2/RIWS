90262b32d9b040f2b9eab98985ae04a267d7fdad
Xaime Pardal <xaime_pardal@outlook.com>
2022-11-18 20:13:17 +0100

    remove logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,6 @@
   <v-app>
     <v-app-bar app flat color="white">
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
         <span class="text-h4"> Reseñas steam </span>
       </div>
 
