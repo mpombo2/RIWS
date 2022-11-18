7ee6069acbe415af83f75ab3f8cffb5beeb556c9
Maria Pombo García <m.pombo2>
2022-11-18 14:55:30 +0100

    Scrapeo en español

--- reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
+++ reviewsGamesSearcher/reviewsGamesSearcher/spiders/steam.py
@@ -6,10 +6,13 @@ from datetime import datetime
 from reviewsGamesSearcher.items import Review
 
 import re
+import locale
 
 
 class SteamSpider(Spider):
     name = "steam"
+    #Idioma para scrapear, 'es' para español e 'en'
+    scrapy_language = "es" 
 
     script = """
 function main(splash, args)
@@ -17,7 +20,9 @@ function main(splash, args)
     local scroll_delay = 1.0
     local scroll_to = splash:jsfunc("window.scrollTo")
     local get_body_height = splash:jsfunc("function() {return document.body.scrollHeight;}")
-    assert(splash:go(splash.args.url))
+    assert(splash:go{splash.args.url, headers={
+        ["Accept-Language"] = splash.args.language,
+    }})
     splash:wait(5.0)
     for _ = 1, splash.args.num_scrolls do
         scroll_to(0, get_body_height())
@@ -30,14 +35,15 @@ end
 
     def start_requests(self):
         urls = [
-            "https://store.steampowered.com/app/813780/Age_of_Empires_II_Definitive_Edition/"]
+            #"https://store.steampowered.com/app/813780/Age_of_Empires_II_Definitive_Edition/"
+            "https://store.steampowered.com/app/431960/Wallpaper_Engine/"]
 
         for url in urls:
-            yield SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'lua_source': self.script, 'timeout': 15, 'num_scrolls': 5})
+            yield SplashRequest(url=url, callback=self.parse, endpoint='execute', args={'lua_source': self.script, 'timeout': 15, 'num_scrolls': 5, 'language': self.scrapy_language})
 
     def parse(self, response):
         href = response.css(".view_all_reviews_btn > a::attr(href)").get()
-        yield SplashRequest(url=href, callback=self.parse_reviews, endpoint='execute', args={'lua_source': self.script, 'timeout': 120, 'num_scrolls': 100})
+        yield SplashRequest(url=href, callback=self.parse_reviews, endpoint='execute', args={'lua_source': self.script, 'timeout': 120, 'num_scrolls': 100, 'language': self.scrapy_language})
 
     def parse_reviews(self, response):
 
@@ -75,7 +81,13 @@ end
             yield review  # Will go to your pipeline
 
     def format_date(self, date): 
-        date = date.replace('Posted ', '')
+        # Para scrapear en ingles
+        if (self.scrapy_language == "en"):
+            date = date.replace('Posted ', '')
+        else:
+            date = date.replace('Publicada el ', '').replace('de ', '')
+            locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+
         if len(date.split()) < 3:
             date = date + " " + datetime.now().strftime("%Y")
 
